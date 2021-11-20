34efc7b97c88ad75b0e8bebd9397b1d78cc92385
Shubham Nivasarkar <74969994+shubhamnivasarkar@users.noreply.github.com>
2021-11-20 09:21:27 +0530

    Create calc.js

---
+++ Node.js/calc.js
@@ -0,0 +1,18 @@
+exports.sum = function(a ,b){
+    return a+b;
+}
+exports.subtration = function(a ,b){
+    return a-b;
+}
+exports.multiplication = function(a ,b){
+    return a*b;
+}
+exports.division = function(a ,b){
+    return a/b;
+}
+exports.sqaure = function(a){
+    return a*a;
+}
+exports.sum1 = function(a ,b,c,d){
+    return a+b+c+d;
+}
